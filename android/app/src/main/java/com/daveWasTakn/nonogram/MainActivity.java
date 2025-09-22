06d2593650aaaa944699d4c0656964eba515ab53
Dave <80040393+DaveWasTakn@users.noreply.github.com>
2025-09-22 18:34:09 +0200

    add android and ios apps using capacitor-js

---
+++ android/app/src/main/java/com/daveWasTakn/nonogram/MainActivity.java
@@ -0,0 +1,5 @@
+package com.daveWasTakn.nonogram;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.daveWasTakn.nonogram',
+  appName: 'Nonogram',
+  webDir: 'dist/nonogram/browser'
+};
+
+export default config;
